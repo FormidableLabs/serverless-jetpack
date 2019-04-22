db24ad5c1e147dc7806be30885f387f112d5c8ca
Ryan Roemer <ryan.roemer@formidable.com>
2019-04-22 12:24:29 -0700

    More research

--- plugins/packager.js
+++ plugins/packager.js
@@ -1,16 +1,33 @@
 "use strict";
 
 class PackagerPlugin {
-  constructor(serverless) {
+  constructor(serverless, options) {
     this.serverless = serverless;
+    this.options = options;
     this.hooks = {
       "before:package:createDeploymentArtifacts": this.package.bind(this)
     };
   }
 
   package() {
+    const { hooks } = this.serverless.pluginManager;
+
+    // RESEARCH: value for `sls package --name THIS_NAME`.
+    const pkgFunctionName = this.options.function;
+
+    // RESEARCH: Find the built-in package service.
+    const slsPackageService = (hooks["package:createDeploymentArtifacts"] || [])
+      .filter((obj) => obj.pluginName === "Package")[0];
+
+    const slsFnPackageService = (hooks["package:function:package"] || [])
+      .filter((obj) => obj.pluginName === "Package")[0];
+
     // eslint-disable-next-line no-console
-    console.log("PKGR: TODO");
+    console.log("PKGR: TODO", {
+      slsPackageService,
+      slsFnPackageService,
+      pkgFunctionName
+    });
   }
 }
 
