f465391c6f70c89c0acb84ee907217a97c066a5a
Ryan Roemer <ryan.roemer@formidable.com>
2019-04-29 22:11:18 -0700

    Start decomposing and adding notes.

--- plugins/packager.js
+++ plugins/packager.js
@@ -1,5 +1,39 @@
 "use strict";
 
+/**
+ * Package Serverless applications manually.
+ *
+ * ## Configuration
+ *
+ * TODO
+ *
+ * ## How it works
+ *
+ * Essentially, the plugin "tricks" Serverless into thinking that an artifact
+ * is specified, which skips all normal Serverless packaging, and then does
+ * the packaging manually.
+ *
+ * Functionally, this looks something like:
+ * 1. Looks for any services and/or functions that the Serverless framework
+ *    would publish with its built-in logic,
+ * 2. Disables this by setting `service|function.pacakge.artifact` fields in
+ *    the runtime Serverless configuration object, and;
+ * 3. Invoking scripts to create an artifact that matches the newly-set
+ *    `.artifact` paths in configuration
+ *
+ * ## Application
+ *
+ * This plugin only is invoked to create custom packages if default Serverless
+ * packaging would apply. Notably, this means this plugin is a noop for
+ * scenarios including:
+ * - `service.package.artifact`
+ * - `function.package.artifact`
+ * - `function.package.disable`
+ *
+ * The plugin does apply and follows the logic of configurations that include:
+ * - `service.package.individually`
+ * - `function.package.individually`
+ */
 class PackagerPlugin {
   constructor(serverless, options) {
     this.serverless = serverless;
@@ -9,14 +43,38 @@ class PackagerPlugin {
     };
   }
 
+  async packageFunction(functionObject) {
+    // eslint-disable-next-line no-console
+    console.log("TODO HERE packageFunction", functionObject);
+
+    // TODO(EXPERIMENT): Check faster with no bundle.
+    if (process.env.TEMP_NO_PACKAGE) {
+      functionObject.package = functionObject.package || {};
+      functionObject.package.artifact = "../../sls-packager-examples-simple.zip";
+    }
+  }
+
+  async packageService() {
+    const { service } = this.serverless;
+    const servicePackage = service.package;
+
+    // eslint-disable-next-line no-console
+    console.log("TODO HERE packageService", {
+      servicePackage,
+      serviceName: service.service
+    });
+
+    // TODO(EXPERIMENT): Check faster with no bundle.
+    if (process.env.TEMP_NO_PACKAGE) {
+      servicePackage.artifact = "../../sls-packager-examples-simple.zip";
+    }
+  }
+
   async package() {
     const { service } = this.serverless;
-    const pkg = service.package;
-    const serviceName = service.service;
+    const servicePackage = service.package;
 
     // Gather internal configuration.
-    const pkgArtifact = pkg.artifact;
-    const pkgIndividually = pkg.individually;
     const fnsPkgs = service.getAllFunctions()
       .map((name) => ({
         name,
@@ -33,38 +91,22 @@ class PackagerPlugin {
         artifact: obj.functionPackage.artifact
       }));
 
-    // We recreate the logic from `packager#packageService`.
-    const shouldPackageService = !pkgIndividually
+    // Now, iterate all functions and decide if this plugin should package them.
+    await Promise.all(fnsPkgs
+      .filter((obj) =>
+        (servicePackage.individually || obj.individually) && !(obj.disable || obj.artifact)
+      )
+      .map((obj) => this.packageService(obj.functionObject))
+    );
+
+    // We recreate the logic from `packager#packageService` for deciding whether
+    // to package the service or not.
+    const shouldPackageService = !servicePackage.individually
       && fnsPkgs.some((obj) => !(obj.disable || obj.individually || obj.artifact));
 
-    // Now, iterate all functions and decide if this plugin should package them.
-    fnsPkgs
-      .filter((obj) => (pkgIndividually || obj.individually) && !(obj.disable || obj.artifact))
-      .forEach((obj) => {
-        // eslint-disable-next-line no-console
-        console.log("TODO HERE FN PACKAGE", obj);
-
-        // TODO(EXPERIMENT): Check faster with no bundle.
-        if (process.env.TEMP_NO_PACKAGE) {
-          obj.functionObject.package = obj.functionObject.package || {};
-          obj.functionObject.package.artifact = "../../sls-packager-examples-simple.zip";
-        }
-      });
-
     // Package entire service if applicable.
-    if (shouldPackageService && !pkgArtifact) {
-      // TODO(EXPERIMENT): Check faster with no bundle.
-      if (process.env.TEMP_NO_PACKAGE) {
-        pkg.artifact = "../../sls-packager-examples-simple.zip";
-      }
-
-      // eslint-disable-next-line no-console
-      console.log("TODO HERE SERVICE PACKAGE", {
-        pkgArtifact,
-        pkgIndividually,
-        shouldPackageService,
-        serviceName
-      });
+    if (shouldPackageService && !servicePackage.artifact) {
+      await this.packageService();
     }
   }
 }
