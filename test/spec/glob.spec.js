11de969cd956c745d2c7d261a0d5d13d3796f9b8
Ryan Roemer <ryan.roemer@formidable.com>
2019-05-03 22:12:16 -0700

    More test setup

--- test/spec/glob.spec.js
+++ test/spec/glob.spec.js
@@ -5,25 +5,65 @@
  * include globbing logic of serverless.
  */
 
+const mock = require("mock-fs");
+
 const Jetpack = require("../..");
 const {
   resolveFilePathsFromPatterns
 } = require("serverless/lib/plugins/package/lib/packageService");
 
+// Serverless mixes in the function, so we do for a mock.
+class Sls {
+  constructor(serverless) {
+    this.serverless = serverless;
+    this.resolveFilePathsFromPatterns = resolveFilePathsFromPatterns;
+  }
+}
+
 describe("globbing (include/exclude) logic", () => {
   let plugin;
+  let sls;
+
+  // Bridge to compare equality between jetpack and serverless.
+  const compare = async ({ pkgInclude, pkgExclude, fnInclude, fnExclude }) => {
+    const pluginError = "No file matches include / exclude patterns";
+    let slsError;
+
+    try {
+      await sls.resolveFilePathsFromPatterns({
+        include: [],
+        exclude: []
+      });
+    } catch (slsErr) {
+      slsError = slsErr;
+    }
+
+    expect(slsError).to.have.property("message", pluginError);
+  };
 
   beforeEach(() => {
-    plugin = new Jetpack({}, {});
-  });
+    mock();
 
-  it("TODO START TEST", () => {
-    console.log({
-      plugin,
-      resolveFilePathsFromPatterns
+    plugin = new Jetpack({}, {});
+
+    sls = new Sls({
+      config: {
+        servicePath: ""
+      },
+      classes: {
+        Error
+      }
     });
   });
 
-  it("should handle empty matches");
-  it("should handle empty patterns");
+  afterEach(() => {
+    mock.restore();
+  });
+
+  it("should error on no patterns, no matches", async () => {
+    await compare({});
+  });
+
+  it("should handle empty matches"); // TODO
+  it("should handle empty patterns"); // TODO
 });
