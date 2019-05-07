213de68723ec7504adee55a9c6974b2523786d1c
Ryan Roemer <ryan.roemer@formidable.com>
2019-05-06 21:06:40 -0700

    All benchmark tests pass now.

--- test/benchmark.js
+++ test/benchmark.js
@@ -15,13 +15,87 @@ const { readFile } = require("fs-extra");
 // Serverless does a traversal and inference of prod vs. dev dependencies
 // throughout installed `node_modules`, but this is tricky to get right and
 // there are known cases of bad matches that we exclude from consideration.
-const BAD_MATCHES = new Set([
+const PKG_IGNORE_ALL = new Set([
   // All binaries.
   "node_modules/.bin"
 ]);
+// False positives from serverless by scneario.
+const SLS_FALSE_POSITIVES = {
+  // For this scenario, it appears that `serverless` doesn't correctly detect
+  // a lot of `jest` dependencies are `devDependencies.
+  "huge/yarn": new Set([
+    // `$ yarn why abbrev`
+    // - "jest#jest-cli#@jest/core#jest-haste-map#fsevents#node-pre-gyp#nopt"
+    "node_modules/abbrev",
 
+    // $ yarn why console-control-strings
+    // - "jest#jest-cli#@jest/core#jest-haste-map#fsevents#node-pre-gyp#npmlog" depends on it
+    // - "jest#jest-cli#@jest/core#jest-haste-map#fsevents#node-pre-gyp#npmlog#gauge"
+    "node_modules/console-control-strings",
+
+    // $ yarn why detect-libc
+    // - "jest#jest-cli#@jest/core#jest-haste-map#fsevents#node-pre-gyp" depends on it.
+    "node_modules/detect-libc",
+
+    //  $ yarn why fs-minipass
+    // - "jest#jest-cli#@jest/core#jest-haste-map#fsevents#node-pre-gyp#tar"
+    "node_modules/fs-minipass",
+
+    // $ yarn why ignore-walk
+    // - "jest#jest-cli#@jest/core#jest-haste-map#fsevents#node-pre-gyp#npm-packlist"
+    "node_modules/ignore-walk",
+
+    // $ yarn why minipass
+    // - "jest#jest-cli#@jest/core#jest-haste-map#fsevents#node-pre-gyp#tar" depends on it
+    // - "jest#jest-cli#@jest/core#jest-haste-map#fsevents#node-pre-gyp#tar#fs-minipass"
+    "node_modules/minipass",
+
+    // $ yarn why minizlib
+    // - "jest#jest-cli#@jest/core#jest-haste-map#fsevents#node-pre-gyp#tar"
+    "node_modules/minizlib",
+
+    // $ yarn why needle
+    // - "jest#jest-cli#@jest/core#jest-haste-map#fsevents#node-pre-gyp"
+    "node_modules/needle",
+
+    // $ yarn why node-pre-gyp
+    // - "jest#jest-cli#@jest/core#jest-haste-map#fsevents"
+    "node_modules/node-pre-gyp",
+
+    // $ yarn why nopt
+    // - "jest#jest-cli#@jest/core#jest-haste-map#fsevents#node-pre-gyp"
+    "node_modules/nopt",
+
+    // $ yarn why npm-bundled
+    // - "jest#jest-cli#@jest/core#jest-haste-map#fsevents#node-pre-gyp#npm-packlist"
+    "node_modules/npm-bundled",
+
+    // $ yarn why npm-packlist
+    // - "jest#jest-cli#@jest/core#jest-haste-map#fsevents#node-pre-gyp"
+    "node_modules/npm-packlist",
+
+    // $ yarn why osenv
+    // - "jest#jest-cli#@jest/core#jest-haste-map#fsevents#node-pre-gyp#nopt"
+    "node_modules/osenv",
+
+    // $ yarn why wide-align
+    // - "jest#jest-cli#@jest/core#jest-haste-map#fsevents#node-pre-gyp#npmlog#gauge"
+    "node_modules/wide-align"
+  ])
+};
+
+// General version
 // eslint-disable-next-line no-magic-numbers
-const notBadMatch = (f) => !BAD_MATCHES.has(f.split("/").slice(0, 2).join("/"));
+const topLevel = (f) => f.split("/").slice(0, 2).join("/");
+
+// Applies to both plugin and baselines
+const keepMatchesAll = (f) => !PKG_IGNORE_ALL.has(topLevel(f));
+
+// Applies only to baselines (false positives).
+const keepBaselineMatch = ({ scenario, mode }) => (f) => {
+  const matches = SLS_FALSE_POSITIVES[`${scenario}/${mode}`];
+  return !matches || !matches.has(topLevel(f));
+};
 
 describe("benchmark", () => {
   let fixtures;
@@ -57,10 +131,12 @@ describe("benchmark", () => {
         // Figure out what is missing from each.
         const missingInBaseline = pluginLines
           .filter((l) => !baselineSet.has(l))
-          .filter(notBadMatch);
+          .filter(keepMatchesAll);
+
         const missingInPlugin = baselineLines
           .filter((l) => !pluginSet.has(l))
-          .filter(notBadMatch);
+          .filter(keepMatchesAll)
+          .filter(keepBaselineMatch({ scenario, mode }));
 
         expect(missingInBaseline, "extra files in jetpack").to.eql([]);
         expect(missingInPlugin, "missing files in jetpack").to.eql([]);
