70cd7fc78167bbdf3d7b425a7c6804ebe329c913
Ryan Roemer <ryan.roemer@formidable.com>
2019-05-29 16:49:49 -0700

    More notes

--- util/bundle.js
+++ util/bundle.js
@@ -2,3 +2,4 @@
 
 // TODO: Expose the normal glob+zip methods
 // TODO: Also expose a worker with lazy requires or something to do in worker?
+// TODO: Probably should move/rename glob.spec.js as it covers the function we're extracting.
