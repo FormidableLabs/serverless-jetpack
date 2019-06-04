664274db92ac2674d27c36f059471e6fa6d33aa2
Ryan Roemer <ryan.roemer@formidable.com>
2019-06-04 13:23:08 -0700

    Add complex CLI tests.

--- test/cli/package.spec.js
+++ test/cli/package.spec.js
@@ -90,4 +90,52 @@ describe("jetpack package", function () {
       expect(await exists(path.join(PKG_DIR, "another.zip"))).to.equal(false);
     });
   });
+
+  describe("complex", () => {
+    const cwd = path.resolve(__dirname, "../packages/complex/yarn");
+    const PKG_DIR = path.join(cwd, ".serverless");
+    const SERVICE_PKG = path.normalize(".serverless/serverless-jetpack-simple.zip");
+    const INDIVIDUALLY_PKG = path.normalize(".serverless/individually.zip");
+    const DISABLED_PKG = path.normalize(".serverless/disabled.zip");
+
+    beforeEach(async () => {
+      await remove(PKG_DIR);
+    });
+
+    it("packages all functions with no options", async () => {
+      const { stdout } = await sls(["jetpack", "package"], { cwd });
+      expect(stdout)
+        .to.contain(`Packaged service: ${SERVICE_PKG}`).and
+        .to.contain(`Packaged function: ${INDIVIDUALLY_PKG}`).and
+        .to.not.contain(`Packaged function: ${DISABLED_PKG}`);
+
+      expect(await exists(path.join(cwd, SERVICE_PKG))).to.equal(true);
+      expect(await exists(path.join(cwd, INDIVIDUALLY_PKG))).to.equal(true);
+      expect(await exists(path.join(cwd, DISABLED_PKG))).to.equal(false);
+    });
+
+    it("packages 1 function with -f individually", async () => {
+      const { stdout } = await sls(["jetpack", "package", "--function", "individually"], { cwd });
+      expect(stdout)
+        .to.contain(`Packaged function: ${INDIVIDUALLY_PKG}`).and
+        .to.not.contain(`Packaged service: ${SERVICE_PKG}`).and
+        .to.not.contain(`Packaged function: ${DISABLED_PKG}`);
+
+      expect(await exists(path.join(cwd, INDIVIDUALLY_PKG))).to.equal(true);
+      expect(await exists(path.join(cwd, SERVICE_PKG))).to.equal(false);
+      expect(await exists(path.join(cwd, DISABLED_PKG))).to.equal(false);
+    });
+
+    it("packages service with -f base", async () => {
+      const { stdout } = await sls(["jetpack", "package", "--function", "base"], { cwd });
+      expect(stdout)
+        .to.contain(`Packaged service: ${SERVICE_PKG}`).and
+        .to.not.contain(`Packaged function: ${INDIVIDUALLY_PKG}`).and
+        .to.not.contain(`Packaged function: ${DISABLED_PKG}`);
+
+      expect(await exists(path.join(cwd, SERVICE_PKG))).to.equal(true);
+      expect(await exists(path.join(cwd, INDIVIDUALLY_PKG))).to.equal(false);
+      expect(await exists(path.join(cwd, DISABLED_PKG))).to.equal(false);
+    });
+  });
 });
