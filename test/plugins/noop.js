0feb01d0af62ce3849e8bace35be8b3b980e8b55
Ryan Roemer <ryan@loose-bits.com>
2025-09-03 10:06:56 -0700

    WIP: Remove plugins

--- test/plugins/noop.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-class NoopPlugin {
-  constructor(serverless) {
-    serverless.cli.log("[noop-plugin]");
-  }
-}
-
-module.exports = NoopPlugin;
--- test/plugins/wrapper.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-// Simple wrapper to runtime swap plugins.
-const Jetpack = require("../../index");
-const NoopPlugin = require("./noop");
-
-// If we're in `deps` or `trace` mode, then use Jetpack
-module.exports = process.env.MODE !== "baseline" ? Jetpack : NoopPlugin;
